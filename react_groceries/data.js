af9e5051ee23f7b33fff85e571b824e7a5b83fdd
Yurong <71139686+Yurong91@users.noreply.github.com>
2022-06-14 07:59:48 -0500

    Add files via upload

---
+++ react_groceries/data.js
@@ -0,0 +1,23 @@
+const list = [
+    {
+        item:'Item1',
+        brand:'brand1',
+        units:'lb',
+        quantity:'1',
+        isPurchased: false,
+    },
+    {
+        item:'Item2',
+        brand:'brand2',
+        units:'lb',
+        quantity:'7',
+        isPurchased: true,
+    },
+    {
+        item:'Item3',
+        brand:'brand3',
+        units:'Pack',
+        quantity:'9',
+        isPurchased: false,
+    }
+]
