9a7d713c4012247f19ed0180610ddd77b731fb80
Yurong <71139686+Yurong91@users.noreply.github.com>
2022-06-17 16:34:43 -0500

    Add files via upload
    handleChange and handleSubmit updated.

--- react_groceries/App.js
+++ react_groceries/App.js
@@ -18,7 +18,22 @@ class App extends React.Component {
     handleSubmit = (e) => {
         e.preventDefault();
         // console.log(e);
-        
+        const newItem = {
+            item: this.state.item,
+            brand: this.state.brand,
+            units: this.state.units,
+            quantity: this.state.quantity,
+            isPurchased: false
+        }
+
+        const list =[...this.state.list, newItem]
+        this.setState({
+            list: list,
+            item: '',
+            brand:'',
+            units:'',
+            quantity: 0
+        })
         
     }
 
@@ -33,13 +48,13 @@ class App extends React.Component {
                     <label htmlFor="name">Item:</label>
                     <input type="text" value={this.state.item} onChange={this.handleChange} id="item"></input>
                     <label htmlFor="brand">Brand:</label>
-                    <input type="text"  id="brand"></input>
+                    <input type="text"  id="brand" value={this.state.brand} onChange={this.handleChange}></input>
                     <label htmlFor="units">Units:</label>
-                    <input type="text" id="units"></input>
+                    <input type="text" id="units" value={this.state.units} onChange={this.handleChange}></input>
                     <label htmlFor="quantity">Quantity:</label>
-                    <input type="Number" min="0" id="quantity"></input>
+                    <input type="Number" min="0" id="quantity" value={this.state.quantity} onChange={this.handleChange}></input>
                     <label htmlFor="isPurchased">isPurchased:</label>
-                    <input type="checkbox" id="isPurchased" ></input>
+                    <input type="checkbox" id="isPurchased" value={this.state.isPurchased} onChange={this.handleChange}></input>
                     <input type="submit"></input>
                     </ul>
                 </form>
